305396ae5f4cfeb72fb98b38ee2214d5dd199a0c
LevelupedHero <127389021+LevelupedHero@users.noreply.github.com>
2024-12-06 15:35:30 +0500

    Подготовил код для работы с БД
    - Скачал необходимые пакеты

---
+++ Data/CalculatorContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Calculator.Data
+{
+    public class CalculatorContext : DbContext
+    {
+        public DbSet<DataInputVariant> DataInputVariants { get; set; }
+
+        public CalculatorContext(DbContextOptions<CalculatorContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //OnModelCreating(modelBuilder);
+        }
+    }
+}
